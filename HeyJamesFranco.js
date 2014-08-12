ee9538385e392ec877e863e9f181d996d031f50f
Alex Choi <heyalexchoi@gmail.com>
2014-08-12 15:17:47 -0400

    revising franco to use promises. having issues with console log waiting until previous promise array fulfills

--- HeyJamesFranco.js
+++ HeyJamesFranco.js
@@ -3,102 +3,98 @@ console.log("Hey James Franco");
 
 var env = require('node-env-file');
 var Twit = require('twit');
+var Q = require('q');
 
 env(__dirname + '/.env');
 
 // credentials stored in local, uncommitted .env file
 var T = new Twit({
-    consumer_key:         process.env.TWITTER_CONSUMER_KEY 
-  , consumer_secret:      process.env.TWITTER_CONSUMER_SECRET
-  , access_token:         process.env.TWITTER_ACCESS_TOKEN
-  , access_token_secret:  process.env.TWITTER_ACCESS_TOKEN_SECRET
+	consumer_key:         process.env.TWITTER_CONSUMER_KEY 
+	, consumer_secret:      process.env.TWITTER_CONSUMER_SECRET
+	, access_token:         process.env.TWITTER_ACCESS_TOKEN
+	, access_token_secret:  process.env.TWITTER_ACCESS_TOKEN_SECRET
 });
 
-// search twitter for (count) number of replies to @jamesfrancotv. executes callback function on array of tweet results
-function getReplies(count, repliesCallback) {
-	T.get('search/tweets', { q:"jamesfrancotv", "in_reply_to_screen_name":'jamesfrancotv', count: count}, function(error, data, response) {
-	if (error !== null) {
-		console.error(Date() + 'getReplies error: ' + error);		
-	} else {
-		repliesCallback(data.statuses);	
-	}
-});	
-}
-// search twitter for (count) number of tweets to @jamesfrancotv. executes callback function on array of tweet results
-function getMentions(count, callback) {
-	T.get('search/tweets', { q:"@jamesfrancotv", count: count}, function(error, data, response) {
-	if (error !== null) {
-		console.error(Date() + 'getMentions error: ' + error);		
-	} else {
-		callback(data.statuses);	
-	}
-});	
-}
-// retweets tweet with tweetID, then executes callback function. 
-// tweetCount and errorCount tally success/failure
-function reTweetID(tweetID, callback, tweetCount, errorCount) {	
-	T.post('statuses/retweet/:id', { id: tweetID.toString() }, function (err, data, response) {
-  		if (err !== null) {
-  			errorCount ++;
-  			console.error(Date() + 'retweet error #' + errorCount + ' err.code: '
-				      + err.code + ' err.message: ' + err.message + ' response status code: ' +  response.statusCode);
-  		}
-  		else {
-  			tweetCount ++;  			
-  		}
-  		callback(tweetCount, errorCount);
-});
-}
-
-function favoriteTweetID(tweetID, callback, favoriteCount, errorCount) {
-	T.post('favorites/create', { id: tweetID.toString() }, function (error, data, response) {
-		if (error) {
-			errorCount ++;
-			console.error(Date() + 'favorite error #' + errorCount + ' error code: ' + error.code + 'error message: ' 
-				+ error.message);
-		} else {
-			favoriteCount ++;
-		}
-		callback(favoriteCount, errorCount);
+// search twitter for (count) number of tweets to @jamesfrancotv. returns promise that fullfills with array of tweets
+function getMentions(count) {
+	return Q.Promise(function (resolve, reject) {
+		T.get('search/tweets', { q: "@jamesfrancotv", count: count }, function(error, data, response) {
+			if (error) reject(error);
+			else resolve(data.statuses);
+		});
 	});
 }
-// recursively calls reTweetID on array of tweets. logs tweets and errors on completion.
-function reTweetTweets(tweets, tweetCount, errorCount, favoriteCount) {
-	if (tweets.length < 1) {
-		console.log(Date() + 'tweeted: ' + tweetCount + 'times. ' + 'favorited: ' + favoriteCount + 'times. ' + errorCount + ' errors');
-		return;
-	}
-	var tweet = tweets.pop();	
-	favoriteTweetID(tweet.id_str, function(){}, favoriteCount, errorCount);
-	reTweetID(tweet.id_str, function(tweetCount,errorCount) {
-		reTweetTweets(tweets, tweetCount, errorCount, favoriteCount);
-	}, tweetCount, errorCount);
-	
+
+// retweets tweet with tweetID, asynchronously returns response via callback.
+function reTweetID(tweetID) {
+	return Q.Promise(function (fullfill, reject) {
+		T.post('statuses/retweet/:id', { id: tweetID.toString() }, function(error, data, response) {
+			if (error) reject(error);
+			else fullfill(data);
+		});
+	});	
+}
+
+function favoriteTweetID(tweetID) {
+	return Q.Promise(function (fullfill, reject) {
+		T.post('favorites/create', { id: tweetID.toString() }, function(error, data, response) {
+			if (error) reject(error);
+			else fullfill(data);
+		});	
+	});
 }
 
 // retweets (count) number of replies to @jamesfrancotv's tweets
-function sayHeyToJames(count){
-//getReplies(count, function (replies) {
-getMentions(count, function (tweets) {
-	var tweetCount = 0;
-	var errorCount = 0;
+function sayHeyToJames(count) {
 	var favoriteCount = 0;
-	reTweetTweets(tweets, tweetCount, errorCount, favoriteCount);
-});
+	var retweetCount = 0;
+	var favoriteErrorCount = 0;
+	var retweetErrorCount = 0;
+	// get all the mentions
+	getMentions(count)
+	// retweet and favorite all of them
+	.then(function(tweets) {
+		var retweetPromises = tweets.map(function(tweet) {
+			reTweetID(tweet.id_str).then(function(data) {
+				retweetCount ++;
+			})
+			.fail(function(error) {
+				retweetErrorCount ++;
+				console.error(Date() + error.allErrors);
+			})
+			.done();
+		});
+		var favoritePromises = tweets.map(function(tweet) {
+			favoriteTweetID(tweet.id_str).then(function(data) {
+				favoriteCount ++;
+			})
+			.fail(function(error) {
+				favoriteErrorCount ++;
+				console.error(Date() + error.allErrors);
+			})
+			.done();
+		});
+		var bothPromises = favoritePromises.concat(retweetPromises);
+		return Q.allSettled(bothPromises);
+		})
+	.then(function() {
+		console.log('\n' + Date() + '\nretweeted: ' + retweetCount + '\nfavorited: ' + favoriteCount 
+			+ '\n retweet errors: ' + retweetErrorCount + '\n favorite errors: ' + favoriteErrorCount);
+	});
 }
 
 if(require.main == module) {
-    console.error(Date() + 'Invoked at command line.');
-    var args = process.argv;
+	console.error(Date() + 'Invoked at command line.');
+	var args = process.argv;
     // can take positive numerical argument to say hey to james that many times
     if (args.length != 3 || Number.isNaN(args[2]) || args[2] < 1) {
     	sayHeyToJames(1);  
-    	 	
+
     } else {
     	sayHeyToJames(args[2]);
     }
 } else {
-    console.error(Date() + 'Invoked via library call');
+	console.error(Date() + 'Invoked via library call');
 }
 
 exports.sayHeyToJames = sayHeyToJames;
